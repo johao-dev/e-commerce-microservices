8ebe73776d51a2414bed620b6705ef9bfd0720c2
johaoDev <johaobravoespinoza1501@gmail.com>
2026-05-01 17:19:35 -0500

    Initial Commit: Config & Discovery Server

---
+++ config-server/src/test/java/zuzz/projects/e_commerce/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package zuzz.projects.e_commerce.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
